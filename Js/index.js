1ea8f019bcfa04cdc26b3ee47ced064f7e8f81ab
Arnav Bansal <arnavbansal.8232@gmail.com>
2021-01-12 23:31:44 +0530

    added initial functionalty (image filters)

--- Js/index.js
+++ Js/index.js
@@ -52,3 +52,69 @@ function upload() {
     original_img.drawTo(can2);
     load = true;
 }
+
+//function to convert image pixels according to filter chosen
+function convert(btn, v) {
+    document.getElementById("download_btn").setAttribute('download', img_name + "_" + v);
+    if (original_img == null) {
+        alert("Image not found!");
+        return;
+    }
+    if (!original_img.complete()) {
+        alert("Image loading!");
+        return;
+    }
+    new_img = new SimpleImage(original_img.getWidth(), original_img.getHeight());
+    for (var p of original_img.values()) {
+        new_img.setPixel(p.getX(), p.getY(), p);
+    }
+    if (new_img.values().length > 1000000) {
+        new_img.setSize(1024, 768);
+    }
+
+    if (btn == "but5") {
+        var colorinput = document.getElementById("but5");
+        var hex = colorinput.value.replace("#", "");
+        var bigint = parseInt(hex, 16);
+        var r = (bigint >> 16) & 255;
+        var g = (bigint >> 8) & 255;
+        var b = bigint & 255;
+        document.getElementById("download_btn").setAttribute('download', img_name + "_" + colorinput.value);
+    }
+
+    if (btn == "but7") {
+        new_img.setSize(new_img.getWidth() / 3, new_img.getHeight() / 3);
+    } else if (btn == "but8") {
+        for (var i of original_img.values()) {
+            new_img.setPixel(new_img.getWidth() - 1 - i.getX(), i.getY(), i);
+        }
+    } else {
+        for (var pi of new_img.values()) {
+            var avg = (pi.getRed() + pi.getGreen() + pi.getBlue()) / 3;
+            switch (btn) {
+                case "but1":
+                    doGrey(pi, avg);
+                    break;
+                case "but2":
+                    doRed(pi, avg);
+                    break;
+                case "but3":
+                    doGreen(pi, avg);
+                    break;
+                case "but4":
+                    doBlue(pi, avg);
+                    break;
+                case "but5":
+                    pi.setRed(color(r, avg));
+                    pi.setGreen(color(g, avg));
+                    pi.setBlue(color(b, avg));
+                    break;
+                case "but6":
+                    doRainBow(pi, avg, new_img.getHeight());
+                    break;
+            }
+        }
+    }
+    document.getElementById("p2").style.visibility = "hidden";
+    new_img.drawTo(can2);
+}
